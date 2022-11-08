e69478cca9b2a56cf30ade25ef600a1fbd9ca6e6
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2022-11-08 19:44:18 +0100

    optimalisation sweep rule algorithm

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ from ca import CASim
 
 
 class CASimTest:
-    def __init__(self, width, height):
+    def __init__(self, width, height, k, r, init_row_prob):
         self.sim = CASim()
+        self.sim.init_row_prob = init_row_prob
         self.sim.width = width
         self.sim.height = height
+        self.sim.k = k
+        self.sim.r = r
         self.max_rule_num = self.sim.k ** (self.sim.k ** (2 * self.sim.r + 1))
-        self.average_cycle_list = []
+        self.average_cycle_lengths = []
 
     def run(self):
         t = 0
@@ -20,79 +23,72 @@ class CASimTest:
             t += 1
 
     def sweep_rule(self, N):
+        """Run the simulation for each rule, N times. Compute the average cycle
+        length for each of the N different random intitial confgurations."""
+
         for rule in range(self.max_rule_num):
             self.sim.rule = rule
-            # Run the simulation and search for cycles for N random initial rows
-            average_cycles_row = []
+
+            cycle_lengths = []
             for _ in range(N):
                 self.run()
-                cycles = self.find_cycles()
-                # print(cycles, "\n", self.sim.config, "\n")
-                if cycles == None:
-                    average_cycles_row.append(self.sim.height)
-                else:
-                    average_cycles_row.append(np.mean(cycles))
+                index_dict = self.make_index_dict()
+                cycle_lengths.append(self.cycle_length(index_dict))
 
-            self.average_cycle_list.append(np.mean(average_cycles_row))
+            self.average_cycle_lengths.append(np.mean(cycle_lengths))
 
-    def find_cycles(self):
+    def make_index_dict(self):
+        """Make dictionary with a set of rows as keys and their indexes in the
+        simulation configuration as values.
+
+        Example:
+        The configuration array [[1 1 1] [1 0 0] [1 1 1] [1 1 1] [1 0 0]] will yield
+        index_dict = {(1, 1, 1): [0, 2, 3], (1, 0, 0): [1, 4]}"""
+
         config_list = [tuple(config) for config in self.sim.config.tolist()]
-        config_set = set(config_list)
 
-        if len(config_set) == len(config_list):
-            # This means that each row is unique, so no cycles.
-            return None
+        index_dict = {}
+        for row, index in enumerate(config_list):
+            # Make a new row key in the dictionary or append the
+            # index if the key already existed
+            index_dict.setdefault(index, []).append(row)
 
-        self.count = 0
-        self.counting = False
-        self.total_cycles = []
-        # Loop through config_set and compare with list to find cycles.
-        for test_config in config_set:
+        return index_dict
 
-            self.cycles = []
-            self.count = 0
+    def cycle_length(self, index_dict):
+        """Calculate the average cycle length of a simulation configuration by
+        calculating the differences of the indexes for each row and taking the
+        average."""
 
-            for config in config_list:
+        cycle_lengths = []
+        for _, indexes in index_dict.items():
+            # if the row was an unique row in the simulation configuration, take
+            # the height as the max cycle length.
+            if len(indexes) == 1:
+                cycle_lengths.append(self.sim.height)
+            else:
+                diffs = np.diff(np.array(indexes))
+                cycle_lengths.extend(diffs)
 
-                if config == test_config:
-                    self.start_stop_count()
-
-                if self.counting == True:
-                    self.count += 1
-
-            self.add_total_cycles()
-            self.counting = False
-
-        return self.total_cycles
-
-    def start_stop_count(self):
-        # Negate counting: True -> False and False -> True
-        self.counting = not self.counting
-
-        # Append the count if True -> False, because then a cycle was found
-        if self.counting == False:
-            self.cycles.append(self.count)
-            self.counting = True
-
-        self.count = 0
-
-    def add_total_cycles(self):
-        if self.cycles == []:
-            self.total_cycles.append(self.sim.height)
-        else:
-            self.total_cycles.extend(self.cycles)
+        return np.mean(cycle_lengths)
 
     def plot(self, cycle_lengths):
         rules = [rule for rule in range(self.max_rule_num)]
+        figure = plt.figure()
         plt.ylabel("Average cycle length")
         plt.xlabel("Rule number (k)")
+        plt.title(
+            f"""Average cycle lengths for the 1D CA (k = {self.sim.k}, r = {self.sim.r})
+            for width = {self.sim.width} and height = {self.sim.height}.""",
+            loc="left",
+        )
         plt.xlim(-1, self.max_rule_num + 1)
         plt.plot(rules, cycle_lengths, ".")
+        figure.savefig("cycle_lengths")
         plt.show()
 
 
 if __name__ == "__main__":
-    sim_test = CASimTest(width=10, height=20)
+    sim_test = CASimTest(width=10, height=20, k=2, r=1, init_row_prob=0.4)
     sim_test.sweep_rule(N=10)
-    print(sim_test.average_cycle_list)
-    sim_test.plot(sim_test.average_cycle_list)
+    sim_test.plot(sim_test.average_cycle_lengths)
