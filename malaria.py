1b05c0309e0a4d710c3a769f80678d1c355512f2
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2022-12-02 00:44:10 +0100

    submitted version

--- malaria.py
+++ malaria.py
@@ -1,5 +1,31 @@
+"""
+"Malaria.py"
+- Developed by Jenna de Vries and Derk Niessink
+
+Model for simulating the infectious Malaria disease using a 2D cellular
+automaton. The model allows for testing the effect of a quarantine prevention
+method.
+
+Usage:
+
+- Model parameters can be adjusted in the __innit__ of the class Model
+
+- Simulation parameters can be adjusted under the if __name__ == "__main__": statement.
+    ->  Simulations will be run for no isolation and all the isolation values
+        in the "isolation_times" list. Leave the list empty to only run for no
+        isolation.
+    ->  Set "plotting" is true to save the two plots as "deathcount.png" and
+        "infectedcount.png"
+    ->  Set "visualize" is true to show the live grid while the simulation is
+        running (will impact performance negatively).
+
+- Data of the infected count and death count of every simulation will be saved
+as "simulation{i}.csv". The highest {i} corresponds to no isolation and the others
+are in order of the "isolation_times" list.
+"""
+
+
 import matplotlib.pyplot as plt
-from collections import Counter
 import numpy as np
 import malaria_visualize
 
@@ -10,23 +36,23 @@ class Model:
         width=50,
         height=50,
         nHuman=400,
-        nMosquito=4000,
-        initMosquitoHungry=0.14,
-        initHumanInfected=0.3,
-        humanInfectionProb=0.9, 
+        nMosquito=2000,
+        initMosquitoHungry=0.1,
+        initHumanInfected=0.1,
+        humanInfectionProb=0.9,
         mosquitoInfectionProb=0.9,
         biteProb=0.9,
-        hungryTime=7,
-        lifeSpan = 56,
-        dieNaturalCauses = 1/21915,
-        HumanDieProb=1/14,
-        dieTime=28,
+        hungryTime=10,
+        lifeSpan=49,
+        dieNaturalCauses=1 / 1000,
+        HumanDieProb=1 / 10,
+        sickTime=35,
         isolation=False,
-        incubationPeriod=19
+        isolationTime=10,
+        immuneTime=90,
     ):
         """
         Model parameters
-        Initialize the model with the width and height parameters.
         """
         self.height = height
         self.width = width
@@ -39,23 +65,22 @@ class Model:
         self.lifeSpan = lifeSpan
         self.HumanDieProb = HumanDieProb
         self.dieNaturalCauses = dieNaturalCauses
-        self.dieTime = dieTime
+        self.sickTime = sickTime
         self.isolation = isolation
-        self.incubationPeriod=incubationPeriod
+        self.isolationTime = isolationTime
+        self.immuneTime = immuneTime
 
         """
         Data parameters
         To record the evolution of the model
         """
-        self.human_properties = []
         self.occupied_cells = []
         self.infectedCount = 0
         self.deathCount = 0
-        # etc.
 
         """
         Population setters
-        Make a data structure in this case a list with the humans and mosquitos.
+        Make a list with the humans and mosquitos.
         """
         self.humanPopulation = self.set_human_population(initHumanInfected)
         self.mosquitoPopulation = self.set_mosquito_population(initMosquitoHungry)
@@ -77,20 +102,19 @@ class Model:
                 self.infectedCount += 1
             else:
                 state = "S"  # S for susceptible
-            humanPopulation.append(Human(x, y, state))
-            self.human_properties.append((x, y, state, 0))
+            humanPopulation.append(
+                Human(
+                    x,
+                    y,
+                    state,
+                    self.sickTime,
+                    self.isolationTime,
+                    self.isolation,
+                    self.immuneTime,
+                )
+            )
 
         return humanPopulation
-
-    def get_free_position(self):
-        x = np.random.randint(self.width)
-        y = np.random.randint(self.height)
-
-        while (x, y) in self.occupied_cells:
-            x = np.random.randint(self.width)
-            y = np.random.randint(self.height)
-
-        return x, y
 
     def set_mosquito_population(self, initMosquitoHungry):
         """
@@ -103,12 +127,36 @@ class Model:
         for i in range(self.nMosquito):
             x = np.random.randint(self.width)
             y = np.random.randint(self.height)
+
             if (i / self.nMosquito) <= initMosquitoHungry:
                 hungry = True
             else:
                 hungry = False
-            mosquitoPopulation.append(Mosquito(x, y, hungry, self.lifeSpan))
+            mosquitoPopulation.append(
+                Mosquito(
+                    x,
+                    y,
+                    hungry,
+                    self.lifeSpan,
+                    self.mosquitoInfectionProb,
+                    self.hungryTime,
+                )
+            )
         return mosquitoPopulation
+
+    def get_free_position(self):
+        """
+        Get a free position in the grid. A free position is defined by a
+        position that does not contain a human.
+        """
+        x = np.random.randint(self.width)
+        y = np.random.randint(self.height)
+
+        while (x, y) in self.occupied_cells:
+            x = np.random.randint(self.width)
+            y = np.random.randint(self.height)
+
+        return x, y
 
     def update(self):
         """
@@ -120,11 +168,13 @@ class Model:
             population, and add a replacement human.
         """
 
-        # Update musquito population
+        """
+        Update musquito population
+        """
         for i, m in enumerate(self.mosquitoPopulation):
 
             m.move(self.height, self.width)
-            m.update_hungriness(self.hungryTime)
+            m.update_hungriness()
             m.update_infectioness()
 
             for h in self.humanPopulation:
@@ -133,44 +183,55 @@ class Model:
                     and m.hungry
                     and np.random.uniform() <= self.biteProb
                 ):
-                    m.bite(h, self.humanInfectionProb, self.mosquitoInfectionProb)
+                    m.bite(h, self.humanInfectionProb)
 
-        # Update human population
+        """"
+        Update human population
+        """
         self.infectedCount = 0
         for j, h in enumerate(self.humanPopulation):
 
-            h.update_sickness(self.dieTime, self.incubationPeriod, self.isolation)
+            h.update_sickness()
+            h.update_immunity()
+
+            if np.random.uniform() <= self.dieNaturalCauses:
+                self.replace_human(j)
 
             if h.state == "I" or h.state == "Q":
                 self.infectedCount += 1
 
                 if np.random.uniform() <= self.HumanDieProb:
-                    # Remove dead  human
-                    self.humanPopulation.pop(j)
-                    self.occupied_cells.pop(j)
+                    self.replace_human(j)
                     self.deathCount += 1
-
-                    # Add newborn human
-                    x, y = self.get_free_position()
-                    self.humanPopulation.append(Human(x, y, "S"))
-                    self.occupied_cells.append((x, y))
-
-            if np.random.uniform() <= self.dieNaturalCauses:
-                # Remove dead  human, nothing is added to deathcount
-                self.humanPopulation.pop(j)
-                self.occupied_cells.pop(j)
-
-                # Add newborn human
-                x, y = self.get_free_position()
-                self.humanPopulation.append(Human(x, y, "S"))
-                self.occupied_cells.append((x, y))               
 
         return self.infectedCount, self.deathCount
 
+    def replace_human(self, index):
+        """
+        Replace a human in the population with a human in state susceptible
+        and with a new position.
+        """
+        self.humanPopulation.pop(index)
+        self.occupied_cells.pop(index)
+
+        x, y = self.get_free_position()
+        self.humanPopulation.append(
+            Human(
+                x,
+                y,
+                "S",
+                self.sickTime,
+                self.isolationTime,
+                self.isolation,
+                self.immuneTime,
+            )
+        )
+        self.occupied_cells.append((x, y))
+
 
 class Mosquito:
-    def __init__(self, x, y, hungry, lifeSpan):
-        """from collections import Counter
+    def __init__(self, x, y, hungry, lifeSpan, mosquitoInfectionProb, hungryTime):
+        """
         Class to model the mosquitos. Each mosquito is initialized with a random
         position on the grid. Mosquitos can start out hungry or not hungry.
         All mosquitos are initialized infection free (this can be modified).
@@ -180,8 +241,10 @@ class Mosquito:
         self.infected = False
         self.time_not_hungry = 0
         self.lifeSpan = lifeSpan
+        self.InfectionProb = mosquitoInfectionProb
+        self.hungryTime = hungryTime
 
-    def bite(self, human, humanInfectionProb, mosquitoInfectionProb):
+    def bite(self, human, humanInfectionProb):
         """
         Function that handles the biting. If the mosquito is infected and the
         target human is susceptible, the human can be infected.
@@ -193,23 +256,30 @@ class Mosquito:
             if np.random.uniform() <= humanInfectionProb:
                 human.state = "I"
         elif not self.infected and human.state == "I":
-            if np.random.uniform() <= mosquitoInfectionProb:
+            if np.random.uniform() <= self.InfectionProb:
                 self.infected = True
         self.hungry = False
         self.time_not_hungry = 0
 
-    def update_hungriness(self, hungryTime):
-        if self.hungry == False:
+    def update_hungriness(self):
+        """
+        If the mosquito is not hungry, update the time that the mosquito has
+        been not hungry and let it be hungry after the given hungry time.
+        """
+        if not self.hungry:
             self.time_not_hungry += 1
-            if self.time_not_hungry > hungryTime:
+            if self.time_not_hungry > self.hungryTime:
                 self.hungry = True
 
     def update_infectioness(self):
-        chanceToDie = 1/self.lifeSpan
+        """
+        Change the mosquito from infected to not infected with a probability
+        dependent on the life time of a mosquito.
+        """
+        chanceToDie = 1 / self.lifeSpan
         rand = np.random.rand()
         if rand < chanceToDie:
             self.infected = False
-    
 
     def move(self, height, width):
         """
@@ -224,74 +294,140 @@ class Mosquito:
 
 
 class Human:
-    def __init__(self, x, y, state):
+    def __init__(self, x, y, state, sickTime, isolationTime, isolation, immuneTime):
         """
         Class to model the humans. Each human is initialized with a random
-        position on the grid. Humans can start out susceptible or infected
-        (or immune).
+        position on the grid. Humans can start out susceptible or infected.
         """
         self.position = [x, y]
         self.state = state
         self.time_sick = 0
+        self.time_immune = 0
+        self.sickTime = sickTime
+        self.isolationTime = isolationTime
+        self.isolation = isolation
+        self.immuneTime = immuneTime
 
-    def update_sickness(self, sickTime, incubationPeriod, isolation):
+    def update_sickness(self):
+        """
+        If human is sick, update the time that it has been sick. Set human in
+        quarantine after the given time and set human to immune if it has
+        survived the disease.
+        """
         if self.state == "I" or self.state == "Q":
             self.time_sick += 1
 
-            if self.time_sick > incubationPeriod and isolation==True:
-                self.state = "Q"
+            if self.time_sick > self.isolationTime and self.isolation == True:
+                self.state = "Q"  # Q for quarantine
 
-            if self.time_sick > sickTime:
-                self.state = "Imm"
+            if self.time_sick > self.sickTime:
+                self.state = "Imm"  # Imm for immune
+                self.time_sick == 0
+
+    def update_immunity(self):
+        """
+        If the human is immune, update the time that it has been immune and
+        change its state to suscepible after the given immune time.
+        """
+        if self.state == "Imm":
+            self.time_immune += 1
+
+            if self.time_immune > self.immuneTime:
+                self.state = "S"
+                self.time_immune = 0
 
 
 if __name__ == "__main__":
     """
     Simulation parameters
     """
-    fileName = "simulation"
-    timeSteps = 800
-    t = 0
+    isolation_times = [2, 5, 9, 19]
     plotData = True
+    visualize = False
+    timeSteps = 2000
+
     """
-    Run a simulation for an indicated number of timesteps.
+    Run simulations without isolation and for the given isolation times
     """
-    file = open(fileName + ".csv", "w")
-    sim = Model()
-    vis = malaria_visualize.Visualization(sim.height, sim.width)
-    print("Starting simulation")
-    while t < timeSteps:
-        [d1, d2] = sim.update()  # Catch the data
-        line = (
-            str(t) + "," + str(d1) + "," + str(d2) + "\n"
-        )  # Separate the data with commas
-        file.write(line)  # Write the data to a .csv file
-        vis.update(t, sim.mosquitoPopulation, sim.humanPopulation)
-        t += 1
-        print(t)
-    file.close()
-    vis.persist()
+    for i in range(len(isolation_times) + 1):
+        fileName = f"simulation{i}"
+        t = 0
+        file = open(fileName + ".csv", "w")
+        if i == len(isolation_times):
+            sim = Model(isolation=False)
+        else:
+            sim = Model(isolation=True, isolationTime=isolation_times[i])
+
+        if visualize:
+            vis = malaria_visualize.Visualization(sim.height, sim.width)
+        print(f"Running simulation {i}")
+
+        """
+        Run the simulation for the given timesteps
+        """
+        while t < timeSteps:
+            [d1, d2] = sim.update()  # Catch the data
+            line = str(t) + "," + str(d1) + "," + str(d2) + "\n"
+            file.write(line)
+
+            if visualize:
+                vis.update(t, sim.mosquitoPopulation, sim.humanPopulation)
+            t += 1
+        file.close()
+        if visualize:
+            vis.persist()
 
     if plotData:
         """
-        Make a plot by from the stored simulation data.
+        Make a plot from the stored simulation data.
         """
-        data = np.loadtxt(fileName + ".csv", delimiter=",")
-        time = data[:, 0]
-        infectedCount = data[:, 1]
-        deathCount = data[:, 2]
-        percentageInfectedCount = [_ / sim.nHuman for _ in data[:, 1]]
+
+        data_no_isolation = np.loadtxt(
+            f"simulation{len(isolation_times)}.csv", delimiter=","
+        )
+        time = data_no_isolation[:, 0]
+        infectedCount = data_no_isolation[:, 1]
+        deathCount = data_no_isolation[:, 2]
+
         plt.figure()
-        plt.plot(time, infectedCount, label="infected")
-        plt.plot(time, deathCount, label="deaths")
-        plt.xlabel("Time")
-        plt.ylabel("Number of humans")
+
+        # Plot deathcount without isolation
+        plt.plot(time, deathCount, label="No isolation")
+
+        # Plot data with several values of isolation times
+        for i, isolation_time in enumerate(isolation_times):
+            data_isolation = np.loadtxt(f"simulation{i}.csv", delimiter=",")
+            deathcount_isolation = data_isolation[:, 2]
+            plt.plot(
+                time,
+                deathcount_isolation,
+                label=f"{isolation_time} days until isolation",
+            )
+
+        plt.xlabel("Time in days")
+        plt.ylabel("Number of human deaths")
         plt.legend()
+        plt.savefig("deathcount.png")
         plt.show()
 
         plt.figure()
-        plt.plot(time, percentageInfectedCount, label="infected")
-        plt.xlabel("Time")
-        plt.ylabel("Percentage of humans")
+        # Plot infected count without isolation
+        percentageInfectedCount = [_ / sim.nHuman for _ in data_no_isolation[:, 1]]
+        plt.plot(time, percentageInfectedCount, label="No isolation")
+
+        # Plot infected count with several values of isolation times
+        for i, isolation_time in enumerate(isolation_times):
+            data_isolation = np.loadtxt(f"simulation{i}.csv", delimiter=",")
+            infectedCount_isolation = data_isolation[:, 1]
+            percentageInfectedCount_iso = [_ / sim.nHuman for _ in data_isolation[:, 1]]
+            plt.plot(
+                time,
+                percentageInfectedCount_iso,
+                label=f"{isolation_time} days until isolation",
+            )
+
+        plt.xlabel("Time in days")
+        plt.ylabel("Percentage of humans infected")
         plt.legend()
-        plt.show()       
+        plt.savefig("infectedcount.png")
+        plt.show()
--- malaria_visualize.py
+++ malaria_visualize.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib
+
 # matplotlib.use('TkAgg') # Mac specific
 import matplotlib.pyplot as plt
 
@@ -14,20 +15,20 @@ class Visualization:
         self.w = width
         self.pauseTime = pauseTime
         grid = np.zeros((self.w, self.h))
-        self.im = plt.imshow(grid, vmin=-4, vmax=2, cmap='rainbow')
+        self.im = plt.imshow(grid, vmin=-4, vmax=2, cmap="rainbow")
         """
         Color information
         """
         fig = plt.gcf()
-        fig.text(0.02, 0.6, 'Free space', color='palegoldenrod', fontsize=14)
+        fig.text(0.02, 0.6, "Free space", color="palegoldenrod", fontsize=14)
 
-        fig.text(0.02, 0.5, 'M: inf', color='red', fontsize=14)
-        fig.text(0.02, 0.45, 'M: not-inf', color='darkorange', fontsize=14)
+        fig.text(0.02, 0.5, "M: inf", color="red", fontsize=14)
+        fig.text(0.02, 0.45, "M: not-inf", color="darkorange", fontsize=14)
 
-        fig.text(0.02, 0.35, 'H: sus', color='aquamarine', fontsize=14)
-        fig.text(0.02, 0.3, 'H: inf', color='darkturquoise', fontsize=14)
-        fig.text(0.02, 0.25, 'H: imm', color='royalblue', fontsize=14)
-        fig.text(0.02, 0.20, 'H: Q', color='blueviolet', fontsize=14)
+        fig.text(0.02, 0.35, "H: sus", color="aquamarine", fontsize=14)
+        fig.text(0.02, 0.3, "H: inf", color="darkturquoise", fontsize=14)
+        fig.text(0.02, 0.25, "H: imm", color="royalblue", fontsize=14)
+        fig.text(0.02, 0.20, "H: Q", color="blueviolet", fontsize=14)
         plt.subplots_adjust(left=0.3)
 
     def update(self, t, mosquitoPopulation, humanPopulation):
@@ -48,11 +49,11 @@ class Visualization:
                 grid[m.position[0]][m.position[1]] = 1
 
         for h in humanPopulation:
-            if h.state == 'S':
+            if h.state == "S":
                 grid[h.position[0]][h.position[1]] = -1
-            elif h.state == 'I':
+            elif h.state == "I":
                 grid[h.position[0]][h.position[1]] = -2
-            elif h.state == 'Q':
+            elif h.state == "Q":
                 grid[h.position[0]][h.position[1]] = -4
             else:
                 grid[h.position[0]][h.position[1]] = -3
@@ -60,7 +61,7 @@ class Visualization:
         self.im.set_data(grid)
 
         plt.draw()
-        plt.title('t = %i' % t)
+        plt.title("t = %i" % t)
         plt.pause(0.01)
 
     def persist(self):
