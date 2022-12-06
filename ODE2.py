88115682da09eb24851aa3d11d469cc047ec6681
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2022-12-06 14:12:16 +0100

    4e

--- ODE2.py
+++ ODE2.py
@@ -4,26 +4,57 @@ import matplotlib.pyplot as plt
 
 def concentration(t, k, g):
     C = np.log(g)
-
     return (np.exp(-k * t + C) - g) / (-k)
 
 
-def plot_concentration(t_range, k, g):
+def plot_concentration(t_range, x, k, g):
 
-    x = [concentration(t, k, g) for t in t_range]
     plt.plot(t_range, x, label=f"g = {g}, k = {k}")
     plt.xlabel("t", fontsize=14)
     plt.ylabel("x(t)", fontsize=14)
     plt.grid(visible=True)
 
 
+def func(x, t, g, k):
+    return g - k * x
+
+
+def euler(stepsize, a, b, initial_condition, func, g, k):
+    x_estimate_list, t_list = [], []
+    x_previous = initial_condition
+    t_previous = a
+    int_sum = 0
+    while a < b + stepsize:
+        x_next = x_previous + stepsize * func(x_previous, t_previous, g, k)
+        x_estimate_list.append(x_previous)
+        t_list.append(a)
+        a += stepsize
+
+        int_sum += (x_next + x_previous) / 2 * stepsize
+
+        x_previous = x_next
+        t_previous += stepsize
+
+    return t_list, x_estimate_list
+
+
 stepsize = 0.001
 t_range = [t for t in np.arange(0, 5 + stepsize, stepsize)]
 
+params = [(2, 3), (1, 1.5), (2, 2), (1, 1)]
+
+
 plt.figure()
-plot_concentration(t_range, g=2, k=3)
-plot_concentration(t_range, g=1, k=1.5)
-plot_concentration(t_range, g=2, k=2)
-plot_concentration(t_range, g=1, k=1)
+for g, k in params:
+    x = [concentration(t, k, g) for t in t_range]
+    plot_concentration(t_range, x, k, g)
 plt.legend()
 plt.savefig("figures/4e.png")
+
+
+plt.figure()
+for g, k in params:
+    t, x = euler(0.001, 0, 5, 0, func, g, k)
+    plot_concentration(t, x, k, g)
+plt.legend()
+plt.savefig("figures/4f.png")
