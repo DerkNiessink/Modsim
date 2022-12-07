8441a6b725ce891a15189d82bf2ea5e410a8cd21
Jenna003 <93924269+Jenna003@users.noreply.github.com>
2022-12-07 21:07:40 +0100

    started exercise 5

--- ODE.py
+++ ODE.py
@@ -1,8 +1,9 @@
-# 2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# a
+"""
+3
+"""
 def func_a(x, t):
     return 1
 
@@ -38,21 +39,6 @@ def euler(stepsize, a, b, initial_condition, func, label, color, alpha, plot_lab
         plt.plot(t_list, x_estimate_list, alpha=alpha, color=color)
 
 
-a, b = 0, 3
-stepsize_list=[1,0.1,0.01]
-alpha_list=[0.15, 0.4, 0.75]
-for index in range(0,len(stepsize_list)):
-
-    alpha = alpha_list[index]
-    plot_label=False
-    if index==2:
-        plot_label=True
-
-    euler(stepsize_list[index], a, b, 0, func_a, r'$\frac{dx}{dt}=1$, $x(0)=0$', 'r', alpha, plot_label)
-    euler(stepsize_list[index], a, b, -4, func_b, r'$\frac{dx}{dt}=2t, x(0)=-4$', 'b', alpha, plot_label)
-    euler(stepsize_list[index], a, b, 4, func_c, r'$\frac{dx}{dt}=-x, x(0)=4$', 'g', alpha, plot_label)
-
-
 def func1(t):
     return t
 
@@ -74,11 +60,65 @@ def analytical(func, plot_label):
         plt.plot(t_list, x_list, alpha=0.5, color='black', label='analytical solution')
     else:
         plt.plot(t_list, x_list, alpha=0.5, color='black')
+
+def exercise_3(): 
+    a, b = 0, 3
+    stepsize_list=[1,0.1,0.01]
+    alpha_list=[0.15, 0.4, 0.75]
+    for index in range(0,len(stepsize_list)):
+
+        alpha = alpha_list[index]
+        plot_label=False
+        if index==2:
+            plot_label=True
+
+        
+        euler(stepsize_list[index], a, b, 0, func_a, r'$\frac{dx}{dt}=1$, $x(0)=0$', 'r', alpha, plot_label)
+        euler(stepsize_list[index], a, b, -4, func_b, r'$\frac{dx}{dt}=2t, x(0)=-4$', 'b', alpha, plot_label)
+        euler(stepsize_list[index], a, b, 4, func_c, r'$\frac{dx}{dt}=-x, x(0)=4$', 'g', alpha, plot_label)
+
+    analytical(func1, plot_label=False)
+    analytical(func2, plot_label=False)
+    analytical(func3, plot_label=True)
+
+    plt.xlabel("t", fontsize=16)
+    plt.ylabel("x(t)", fontsize=16)
+    plt.legend()
+    plt.savefig('3.png')
+
+    pass
+
+# exercise_3()
+# plt.clf()
+
+"""
+5c
+"""
+
+def rabbit_func(r,k,t,N_i):
+    x = N_i * ( np.e**((r-k)*t) )
+    return x
+
+def plot(r,k,N_i):
+    t_list = np.linspace(0,30,1000)
+    x_list=[]
+    for t in t_list:
+        x = rabbit_func(r,k,t,N_i)
+        x_list.append(x)
+    plt.plot(t_list, x_list, label=f"r = {r}, k = {k}")
+    plt.grid(visible=True)
+    plt.xlim(0,30)
+    plt.ylim(-5,30)
+
+    plt.xlabel("t", fontsize=16)
+    plt.ylabel("x(t)", fontsize=16)
     
+    return t_list
 
-analytical(func1, plot_label=False)
-analytical(func2, plot_label=False)
-analytical(func3, plot_label=True)
-
+# plot(2, 1, 25)
+t_list = plot(1, 1, 25)
+plot(1, 2, 25)
+plt.plot(t_list, t_list, label="x(t)=t")
 plt.legend()
-plt.savefig('3.png')
+
+plt.savefig("5c.png")
